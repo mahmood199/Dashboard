2bd80378b06feb51ea1e16ff600cebb6efefd15c
MSI <gaudentas199@gmail.com>
2024-02-06 23:37:00 +0530

    fab finishing

--- app/src/main/java/com/example/composedweather/ui/feature/home_container/BottomNavScreen.kt
+++ app/src/main/java/com/example/composedweather/ui/feature/home_container/BottomNavScreen.kt
@@ -9,6 +9,7 @@ sealed class BottomNavScreen(
 ) {
     data object Links : BottomNavScreen(position = 0, text = "Links", R.drawable.ic_link_2)
     data object Courses : BottomNavScreen(position = 1, text = "Courses", R.drawable.ic_courses)
-    data object Campaigns : BottomNavScreen(position = 2, text = "Campaigns", R.drawable.ic_campaigns)
-    data object Profile : BottomNavScreen(position = 3, text = "Profile", R.drawable.ic_profile)
+    data object AddLinks : BottomNavScreen(position = 2, text = "Add Links", R.drawable.ic_courses)
+    data object Campaigns : BottomNavScreen(position = 3, text = "Campaigns", R.drawable.ic_campaigns)
+    data object Profile : BottomNavScreen(position = 4, text = "Profile", R.drawable.ic_profile)
 }
--- app/src/main/java/com/example/composedweather/ui/feature/home_container/HomeContainerUI.kt
+++ app/src/main/java/com/example/composedweather/ui/feature/home_container/HomeContainerUI.kt
@@ -4,11 +4,18 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -20,6 +27,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
@@ -28,6 +36,7 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.composedweather.ui.feature.dashboard.DashboardUIContainer
 import com.example.composedweather.ui.theme.ComposedWeatherTheme
+import com.example.composedweather.ui.theme.ElectricBlue
 import com.example.composedweather.ui.theme.FigtreeSemiBold
 import com.example.composedweather.ui.theme.SilverChalice
 
@@ -46,29 +55,19 @@ fun HomeContainerUI(
                 buildList {
                     add(BottomNavScreen.Links)
                     add(BottomNavScreen.Courses)
+                    add(BottomNavScreen.Links)
                     add(BottomNavScreen.Campaigns)
                     add(BottomNavScreen.Profile)
                 }
             }
 
-            BottomAppBar(
-                containerColor = Color.White,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 12.dp)
-            ) {
-                bottomBarItem.forEachIndexed { index, screens ->
-                    BottomBarItem(
-                        screen = screens,
-                        isSelected = state.currentPage == index,
-                        onClick = {
-                            viewModel.updateSelectedScreen(it)
-                        },
-                        modifier = Modifier
-                            .weight(1f)
-                    )
+            NavBottomBar(
+                state = state,
+                bottomBarItem = bottomBarItem,
+                onItemClicked = {
+                    viewModel.updateSelectedScreen(it)
                 }
-            }
+            )
 
         },
         modifier = modifier
@@ -80,6 +79,76 @@ fun HomeContainerUI(
                 .padding(bottom = it.calculateBottomPadding() - it.calculateBottomPadding())
         )
     }
+}
+
+@Composable
+fun NavBottomBar(
+    state: HomeContainerViewState,
+    bottomBarItem: List<BottomNavScreen>,
+    onItemClicked: (Int) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    BottomAppBar(
+        actions = {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                bottomBarItem.forEachIndexed { index, screens ->
+                    when (index) {
+                        2 -> {
+                            FloatingActionButton(
+                                shape = CircleShape,
+                                elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation(
+                                    defaultElevation = 12.dp,
+                                    pressedElevation = 3.dp,
+                                    focusedElevation = 8.dp,
+                                    hoveredElevation = 6.dp,
+                                ),
+                                containerColor = ElectricBlue,
+                                onClick = {},
+                                modifier = Modifier
+                                    .clip(CircleShape)
+                                    .background(
+                                        brush = Brush.radialGradient(
+                                            colors = listOf(
+                                                ElectricBlue,
+                                                ElectricBlue.copy(alpha = 0.0625f),
+                                            )
+                                        )
+                                    )
+                                    .padding(8.dp)
+                                    .clip(CircleShape)
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.Add,
+                                    contentDescription = null
+                                )
+                            }
+                        }
+
+                        else -> {
+                            BottomBarItem(
+                                screen = screens,
+                                isSelected = state.currentPage == index,
+                                onClick = {
+                                    onItemClicked(it)
+                                },
+                                modifier = Modifier
+                                    .weight(1f)
+                            )
+
+                        }
+                    }
+                }
+            }
+        },
+        floatingActionButton = {
+        },
+        containerColor = Color.White,
+        modifier = modifier
+            .fillMaxWidth()
+    )
 }
 
 @Composable
@@ -102,7 +171,8 @@ fun BottomBarItem(
         Icon(
             imageVector = ImageVector.vectorResource(screen.drawable),
             contentDescription = null,
-            tint = if (isSelected) Color.Black else SilverChalice
+            tint = if (isSelected) Color.Black else SilverChalice,
+            modifier = Modifier.size(if (isSelected) 32.dp else 24.dp)
         )
 
         Text(
