a1fb1d447bf3f8b316f8d1d1d0c2122a7c42b6d3
MSI <gaudentas199@gmail.com>
2024-02-08 10:52:31 +0530

    final ready

--- app/src/main/java/com/example/composedweather/ui/common/CustomBottomNavigation.kt
+++ app/src/main/java/com/example/composedweather/ui/common/CustomBottomNavigation.kt
@@ -1,23 +1,29 @@
 package com.example.composedweather.ui.common
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.composedweather.R
 import com.example.composedweather.ui.feature.home_container.BottomBarItem
 import com.example.composedweather.ui.feature.home_container.BottomNavScreen
 import com.example.composedweather.ui.theme.ComposedWeatherTheme
+import com.example.composedweather.ui.theme.SilverChalice
 
 @Composable
 fun CustomBottomNavigation(
@@ -31,16 +37,25 @@ fun CustomBottomNavigation(
         modifier = modifier
             .fillMaxWidth()
     ) {
-        Row(
+        Column(
+            verticalArrangement = Arrangement.spacedBy((-0.5).dp),
             modifier = Modifier
                 .fillMaxWidth()
                 .align(Alignment.BottomCenter)
         ) {
 
+            Icon(
+                imageVector = ImageVector.vectorResource(R.drawable.ic_bottom_nav_middle_bulge),
+                contentDescription = null,
+                tint = Color.White,
+                modifier = Modifier
+                    .padding(top = 2.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
+
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .clip(MiddleBulgeShape())
                     .background(Color.White),
                 verticalAlignment = Alignment.CenterVertically
             ) {
@@ -75,7 +90,7 @@ fun CustomBottomNavigation(
             onClick = onFabClicked,
             modifier = Modifier
                 .align(Alignment.TopCenter)
-                .padding(top = 6.dp)
+                .padding(top = 8.dp)
         )
 
     }
@@ -89,7 +104,7 @@ fun CustomBottomNavigationPreview() {
         Box(
             modifier = Modifier
                 .fillMaxSize()
-                .background(Color.Blue)
+                .background(SilverChalice)
         ) {
             val bottomBarItem = remember {
                 buildList {
